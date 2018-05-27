d7c9f1d13bd9318995c058d36d7fa8a0830894a9
DimkaSklyar <DimkaSklyar@mail.ru>
2018-05-27 23:53:07 +0300

    v.0.5.0

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,54 @@
 $(document).ready(function () {
 	
-
+	$('#feedback-email').blur(function(){
+		if($(this).val() != '') {
+			$(this).next().css({'transform': 'translateY(-90px)','font-size': '.5em'})
+		} else {
+			$(this).next().removeAttr( 'style' );
+		}
+	});
+	
+	$('.owl-carousel').owlCarousel({
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:true
+			},
+			600:{
+				items:3,
+				nav:false
+			},
+			1000:{
+				items:2,
+				nav:true,
+				loop:false
+			}
+		}
+	})
+	
+$('.service-items').animated('fadeIn','fadeOut');
 
 });
+
+(function($) {
+		$.fn.animated = function(inEffect, outEffect) {
+				$(this).css("opacity", "0").addClass("animated").waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						} else {
+								$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+						};
+				}, {
+						offset: "80%"
+				}).waypoint(function(dir) {
+						if (dir === "down") {
+								$(this).removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+						} else {
+								$(this).removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+						};
+				}, {
+						offset: -$(window).height()
+				});
+		};
+})(jQuery);
